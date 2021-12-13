a9d2bae2f3a3a827a72050b7a9dc5801a0d3dbe1
JulioAndres2021 <94810568+JulioAndres2021@users.noreply.github.com>
2021-12-13 18:26:15 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,19 @@
+/*const arreglo =[5,10,21,13,1]
+
+ function buscar(item) {
+     return item > 20
+  
+   
+}
+console.log(arreglo.find(buscar))*/
+
+
+function calcular() {
+    var num1 = parseint(document.getElementById("numero1").value);
+     var num2 = parseint(document.getElementById("numero2").value);
+     var num3 = parseint(document.getElementById("numero3").value);
+     var num4 = parseint(document.getElementById("numero4").value);
+     var num5 = parseint(document.getElementById("numero5").value);
+        var mayor = math.max(num1, num2, num3, num4, num5)
+        alert(mayor);
+};
